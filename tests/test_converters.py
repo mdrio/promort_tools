0e684df5f9eacf93ac7301bedf1ef486f4044a01
Mauro Del Rio <mauro@crs4.it>
2021-09-17 12:23:11 +0200

    add test for mask to shapes

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -1,13 +1,29 @@
 from promort_tools.converters.mask_to_shapes import convert_to_shapes
 
 
-def test_mask_to_roi_th_0(mask_data):
+def test_mask_to_shapes_th_0(mask_data):
     mask, orig_res = mask_data
     shapes = convert_to_shapes(mask, orig_res, -1)
-    print(shapes)
     assert len(shapes) == 1
     coordinates = shapes[0]['coordinates']
+    assert len(coordinates) == 5
+    print(coordinates)
     assert coordinates[0] == (0, 0)
-    assert coordinates[0] == (0, orig_res[1] - 1)
-    assert coordinates[0] == (orig_res[0] - 1, orig_res[1] - 1)
-    assert coordinates[0] == (orig_res[0] - 1, 0)
+    assert coordinates[1] == (0, orig_res[1] - 1)
+    assert coordinates[2] == (orig_res[0] - 1, orig_res[1] - 1)
+    assert coordinates[3] == (orig_res[0] - 1, 0)
+    assert coordinates[4] == (0, 0)
+
+
+def test_mask_to_shapes_th_100(mask_data):
+    mask, orig_res = mask_data
+    shapes = convert_to_shapes(mask, orig_res, 100)
+    assert len(shapes) == 1
+    coordinates = shapes[0]['coordinates']
+    assert len(coordinates) == 5
+    print(coordinates)
+    assert coordinates[0] == (0, 0)
+    assert coordinates[1] == (0, orig_res[1] // 2 - 1)
+    assert coordinates[2] == (orig_res[0] // 2 - 1, orig_res[1] // 2 - 1)
+    assert coordinates[3] == (orig_res[0] // 2 - 1, 0)
+    assert coordinates[4] == (0, 0)
