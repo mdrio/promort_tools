96d46a0f76ae468e7d9872312e467ce4d26f8a7f
Mauro Del Rio <mauro@crs4.it>
2021-09-20 12:15:17 +0200

    refactor square mask fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,7 @@ import pytest
 
 @pytest.fixture
 def square_mask() -> Tuple[np.ndarray, Tuple[int, int]]:
-    orig_res = np.array([32, 32])
-    level_downsample = 2
-    shape = orig_res // level_downsample
+    shape = [16, 16]
     mask = np.zeros(shape, dtype='uint8')
     mask[:shape[0] // 2, :shape[1] // 2] = 50
     mask[shape[0] // 4:shape[0] // 2, shape[1] // 4:shape[1] // 2] = 100
